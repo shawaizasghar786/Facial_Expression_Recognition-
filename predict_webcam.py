972611a0a08fd1035f9bc329d562c0db90d5297a
Shawaiz Asghar <112537364+shawaizasghar786@users.noreply.github.com>
2025-09-24 01:34:27 +0500

    predict_webcam complete

---
+++ predict_webcam.py
@@ -0,0 +1,28 @@
+import cv2, torch
+from PIL import Image
+from torchvision import transforms
+from model import EmotionCNN
+
+def run_webcam(class_names):
+    model = EmotionCNN(num_classes=len(class_names))
+    model.load_state_dict(torch.load('model/emotion_model.pth'))
+    model.eval()
+
+    transform = transforms.Compose([
+        transforms.Grayscale(), transforms.Resize((48, 48)), transforms.ToTensor()
+    ])
+    cap=cv2.VideoCapture(0)
+    while True:
+        ret, frame=cap.read()
+        gray=cv2.cvtColor(frame,cv2.Color_BGR2GRAY)
+        face=cv2.resize(gray,(48,48))
+        tensor=transform(Image.fromarray(face)).unsqueez(0)
+        output=model(tensor)
+        pred = torch.argmax(output, dim=1).item()
+        cv2.putText(frame, class_names[pred], (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+        cv2.imshow('Emotion Detection', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+        
