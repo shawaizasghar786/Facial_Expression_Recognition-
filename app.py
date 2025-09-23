cef497a5c3f4e1376779860ede801ec939f0ed99
Shawaiz Asghar <112537364+shawaizasghar786@users.noreply.github.com>
2025-09-24 01:40:01 +0500

    app done

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from scripts.predict_image import predict_image
+from scripts.predict_webcam import run_webcam
+from scripts.utils import get_dataloaders
+
+_, _, class_names = get_dataloaders()
+
+st.title("Facial Expression Recognition")
+
+mode = st.radio("Choose input mode:", ["Image Upload", "Webcam Stream"])
+
+if mode == "Image Upload":
+    uploaded_file=st.file_uploader("Upload an image",type=["jpg","png"])
+    if uploaded_file:
+        st.image(uploaded_file)
+        predict_image(uploaded_file,class_names)
+elif mode=="Webcam Stream":
+st.write("Press 'Start' to begin webcam emotion detection.")
+if st.button("Start"):
+    run_webcam(class_names)
