2998bf7dc8f5bd35e2614bd782c0ce13536cac79
Shawaiz Asghar <112537364+shawaizasghar786@users.noreply.github.com>
2025-09-25 00:38:54 +0500

    path file save error resolve

--- train.py
+++ train.py
@@ -21,4 +21,4 @@ for epoch in range(15):
         loss.backward()
         optimizer.step()
     print(f"Epoch {epoch+1}, Loss: {loss.item(): .4f}")
-torch.save(model.state_dist(),'model/emotion_model.pth')
+torch.save(model.state_dict(), 'model/emotion_model.pth')
